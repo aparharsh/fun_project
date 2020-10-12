c817cf6f161e57af46bc93baea55a39288121c9b
Tanya Verma <43872256+tanya1526@users.noreply.github.com>
2020-10-12 19:16:03 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,7 +5,12 @@ root = Tk()
 # the size can be changed
 root.geometry("1250x700")
 
-# Here goes the main code
+######## Functions
+
+def printBot():
+	pass
+
+########  Main Code
 
 label1 = Label(root, text="Enter Your Sentence", font=('Arial', 14, 'bold'))
 label1.pack()
@@ -15,5 +20,8 @@ label1.pack()
 
 e1 = Text(root, width=60, height=10, border=3)
 e1.pack()
+
+b1 = Button(root, text="Click Me to Print!", bg="brown", fg="white", font=('Arial', 10), command=printBot)
+b1.pack(pady=10)
 
 mainloop()
